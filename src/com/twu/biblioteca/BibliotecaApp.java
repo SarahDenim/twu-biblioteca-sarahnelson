5607e2c83f0516a987a013b3a22401b8471079b0
Sarah Nelson <sarah.nelson1@uqconnect.edu.au>
2015-04-16 23:36:46 +1000

    Working out menu system

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,8 +1,32 @@
 package com.twu.biblioteca;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class BibliotecaApp {
 
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
+    public static void main(String[] args) throws IOException {
+
+        System.out.println("Welcome to Biblioteca! We're ready to rumble!");
+        System.out.println("Main menu: \n1. List Books \n(use numbers to select menu items) \n\nCommands" +
+                "\nm: show main menu\nl: list books \nco <book-name>: checkout book \nr <book-name>: return book" +
+                " \nc: show commands");
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("What would you like to do?");
+
+        //check if input is a number (selecting menu item) or a command
+
+        try{
+            int i = Integer.parseInt(br.readLine());
+            System.out.println(i);
+        }catch(NumberFormatException nfe){
+            System.err.println("Invalid Format!");
+        }
+        String s = br.readLine();
+        System.out.println(s);
+
+        //use switch statement to process command
     }
 }
--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -2,7 +2,9 @@ package com.twu.biblioteca;
 
 
 import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
+
 
 public class ExampleTest {
 
@@ -10,4 +12,14 @@ public class ExampleTest {
     public void test() {
         assertEquals(1, 1);
     }
+
+
+    /*@Rule
+    public final StandardOutputStreamLog log = new StandardOutputStreamLog();
+
+    @Test
+    public void writesTextToSystemOut() {
+        System.out.println("Hello, world!");
+        assertEquals("Hello, world!", log.getLog());
+    }*/
 }

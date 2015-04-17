da1791d17a1ae9f8273cbf5a90ceb7283994e1b7
Sarah Nelson <sarah.nelson1@uqconnect.edu.au>
2015-04-17 16:03:01 +1000

    Working on checking input and running command

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -8,46 +8,82 @@ import java.util.List;
 
 public class BibliotecaApp {
 
-    private List<Book> bookList = new ArrayList();
+    //private List<Book> bookList = new ArrayList();
 
     public static void main(String[] args) throws IOException {
+        welcomeMessage();
+        mainMenu();
+        runCommand(checkInput());
+    }
 
-        System.out.println("Welcome to Biblioteca! We're ready to rumble!");
-        System.out.println("Main menu: \n1. List Books \n(use numbers to select menu items) \n\nCommands" +
-                "\nm: show main menu\nl: list books \nco <book-name>: checkout book \nr <book-name>: return book" +
-                " \nc: show commands");
-
+    public static char checkInput() throws IOException {
+        System.out.println("\nWhat would you like to do?");
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        System.out.print("What would you like to do?");
 
-        //check if input is a number (selecting menu item) or a command
+        //check if input is a command
 
-        String command = new String();
-        int inputInt;
-        String input;
+        String input = br.readLine();
 
-        input = br.readLine();
-
-        try{
-            //check if input is an integer
-            inputInt = Integer.parseInt(br.readLine());
-            if (inputInt == 1) {
-                System.out.println("listing books");
-                //call list books method
-            }
-            else {
-                System.out.println("Enter valid menu item number");
-            }
-
-        }catch(NumberFormatException nfe){
-            command = input;
-            //run command according to input
-
-            //else error message
-            //System.err.println("Invalid Format!");
+        if(input.equals("q")) {
+            return 'q';
         }
+        else if (input.equals("m")) {
+            return 'm';
+        }
+        else {
+            //check if input is a number (selecting menu item) or a command
+            int inputInt;
 
-        //use switch statement to process command
+            try {
+                //check if input is an integer
+                inputInt = Integer.parseInt(input);
+                if (inputInt == 1) {
+                    return 1;
+                    //call list books method
+                } else {
+                    System.err.println("Enter valid menu item number");
+                    runCommand(checkInput());
+                }
+            } catch (NumberFormatException nfe) {
+                System.err.println("Invalid Command");
+                runCommand(checkInput());
+            }
+        }
+        return 0;
+    }
 
+    public static void runCommand(char command) throws IOException {
+        switch (command) {
+            case 'm':
+                mainMenu();
+                runCommand(checkInput());
+                break;
+            case 'q':
+                quit();
+                runCommand(checkInput());
+                break;
+            case 1:
+                listBooks();
+                runCommand(checkInput());
+        }
+    }
+
+    public static void listBooks() throws IOException {
+        System.out.println("Listing books");
+    }
+
+    public static void quit() throws IOException {
+        //quit program
+        System.out.println("Quitting program");
+    }
+
+    public static void welcomeMessage() throws IOException {
+        System.out.println("Welcome to Biblioteca! We're ready to rumble!");
+    }
+
+    public static void mainMenu() throws IOException {
+        System.out.println("Main menu: \n1. List Books \n2. Checkout book \n" +
+                "3. Return book \n\nCommands (use at any time) \nm: show main menu " +
+                "\nq: quit");
     }
 }
--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -1,25 +1,70 @@
 package com.twu.biblioteca;
 
-
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import java.io.*;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class ExampleTest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+
 
     @Test
     public void test() {
         assertEquals(1, 1);
     }
 
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
 
-    /*@Rule
-    public final StandardOutputStreamLog log = new StandardOutputStreamLog();
+    }
+
+    @After
+    public void cleanUpStreams() {
+        System.setOut(null);
+        System.setErr(null);
+    }
 
     @Test
-    public void writesTextToSystemOut() {
-        System.out.println("Hello, world!");
-        assertEquals("Hello, world!", log.getLog());
-    }*/
+    public void welcomeMessageTest() throws Exception {
+        BibliotecaApp.welcomeMessage();
+        assertEquals("Welcome to Biblioteca! We're ready to rumble!\n", outContent.toString());
+    }
+
+    @Test
+    public void mainMenuTest() throws Exception {
+        BibliotecaApp.mainMenu();
+        assertEquals("Main menu: \n1. List Books \n2. Checkout book \n" +
+                "3. Return book \n\nCommands (use at any time) \nm: show main menu " +
+                "\nq: quit\n", outContent.toString());
+    }
+
+    @Test
+    public void checkInputM() throws Exception {
+        ByteArrayInputStream inContentM = new ByteArrayInputStream("m".getBytes());
+        System.setIn(inContentM);
+        assertEquals('m', BibliotecaApp.checkInput());
+    }
+
+    @Test
+    public void checkInputQ() throws Exception {
+        ByteArrayInputStream inContentM = new ByteArrayInputStream("q".getBytes());
+        System.setIn(inContentM);
+        assertEquals('q', BibliotecaApp.checkInput());
+    }
+
+    @Test
+    public void checkInput1() throws Exception {
+        ByteArrayInputStream inContent1 = new ByteArrayInputStream("1".getBytes());
+        System.setIn(inContent1);
+        assertEquals(1, BibliotecaApp.checkInput());
+    }
 }
